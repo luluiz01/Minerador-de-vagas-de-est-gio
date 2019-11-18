a7c9641cc5cbdcfeda596f0e4e1c9871298b6532
Luiz Henrique <luiz.pventura@gmail.com>
2019-11-18 17:46:49 -0300

    Separando vagas por cidade

--- main.py
+++ main.py
@@ -2,50 +2,58 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import requests
 import time
-#import logging
 
 url = 'https://www.apinfo.com/apinfo/inc/list4.cfm'
 soup = BeautifulSoup(urlopen(url), "html.parser")
-print("**********************************")
 
-#busca o título da vaga
-titulo = []
-for item in soup.select('.cargo'):
-  texto = item.get_text().strip()
-  titulo.append(texto)
-print(titulo)
+time.sleep(2)
 
-print("**********************************")
-#buca o link da vaga
-links = []
-for item in soup.select('.texto'):
-  links.append(item.a.get('href'))
-print(links)
-
-#--
-
-#url = "https://www.apinfo.com/apinfo/inc/list4.cfm"
-time.sleep(5)
 print('\n\n Post \n\n')
+# Faz a filtragem no site e retorna vagas de Brasilia
 filtro = {"cod_cidade[]": 70000}
 post_result = requests.post(url, filtro)
-#get_result = requests.get(url)
-#data = urlopen(url).read()
 soup = BeautifulSoup(post_result.text, "html.parser")
-print("**********************************")
 
-#busca o título da vaga
+print('-' * 82)
+
+# busca a data e a cidade em que a vaga foi publicada
+data = []
+cidade = []
+for item in soup.select('.info-data'):
+    texto = item.get_text().strip().split()
+    data.append(texto[-1])
+    cidade.append(texto[0])
+
+print(data)
+print(cidade)
+
+print('-' * 82)
+
+# busca o título da vaga
 titulo = []
 for item in soup.select('.cargo'):
-  texto = item.get_text().strip()
-  titulo.append(texto)
+    texto = item.get_text().strip()
+    titulo.append(texto)
 print(titulo)
 
-print("**********************************")
-#buca o link da vaga
-links = []
+print('-' * 82)
+
+# busca a descrição da vaga
+descricao = []
+empresas = []
+cods = []
 for item in soup.select('.texto'):
-  links.append(item.a.get('href'))
-print(links)
-#print(post_result.text)
-#print(data)
+    texto = item.find_all('p')[0]
+    descricao.append(texto)
+
+    empresa_cod = item.find_all('p')[1]
+    empresa = list(empresa_cod)[2].strip()
+    empresas.append(empresa)
+    cod = list(empresa_cod)[6].strip()
+    cods.append(cod)
+print(cods)
+print('Depois do I')
+
+for i, v in enumerate(descricao):
+    print(f'{i + 1}º vaga')
+    print(f'{v}', end='\n\n')
