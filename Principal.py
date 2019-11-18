36105a8a0c0d9076e74e6fecbe9a2cc72083324b
Luiz Henrique <luiz.pventura@gmail.com>
2019-11-18 18:46:52 -0300

    Transformando em funções

---
+++ Principal.py
@@ -0,0 +1,7 @@
+import main
+main.filtrarPeloTitulo()
+main.filtrarPelaData()
+main.filtrarPelaCidade()
+main.filtrarPeloCodigo()
+main.filtrarPeloLink()
+main.filtrarPelaDescricao()
--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import requests
-import time
 
 url = 'https://www.apinfo.com/apinfo/inc/list4.cfm'
 soup = BeautifulSoup(urlopen(url), "html.parser")
-
-time.sleep(2)
 
 print('\n\n Post \n\n')
 # Faz a filtragem no site e retorna vagas de Brasilia
@@ -14,46 +11,53 @@ filtro = {"cod_cidade[]": 70000}
 post_result = requests.post(url, filtro)
 soup = BeautifulSoup(post_result.text, "html.parser")
 
-print('-' * 82)
 
-# busca a data e a cidade em que a vaga foi publicada
-data = []
-cidade = []
-for item in soup.select('.info-data'):
-    texto = item.get_text().strip().split()
-    data.append(texto[-1])
-    cidade.append(texto[0])
+def filtrarPelaData():
+    data = []
+    for item in soup.select('.info-data'):
+        texto = item.get_text().strip().split()
+        data.append(texto[-1])
+    return print(data)
 
-print(data)
-print(cidade)
 
-print('-' * 82)
+def filtrarPelaCidade():
+    cidade = []
+    for item in soup.select('.info-data'):
+        texto = item.get_text().strip().split()
+        cidade.append(texto[0])
+    return print(cidade)
+
 
 # busca o título da vaga
-titulo = []
-for item in soup.select('.cargo'):
-    texto = item.get_text().strip()
-    titulo.append(texto)
-print(titulo)
+def filtrarPeloTitulo():
+    titulo = []
+    for item in soup.select('.cargo'):
+        texto = item.get_text().strip()
+        titulo.append(texto)
+    return print(titulo)
 
-print('-' * 82)
 
+def filtrarPeloLink():
+    links = []
+    for item in soup.select('.texto'):
+        links.append(item.a.get('href'))
+    return print(links)
+
+
+def filtrarPeloCodigo():
+    cods = []
+    for item in soup.select('.texto'):
+        empresa_cod = item.find_all('p')[1]
+        cod = list(empresa_cod)[6].strip()
+        cods.append(cod)
+    return print(cods)
+
+
+# obs: precisa melhorar a descrição
 # busca a descrição da vaga
-descricao = []
-empresas = []
-cods = []
-for item in soup.select('.texto'):
-    texto = item.find_all('p')[0]
-    descricao.append(texto)
-
-    empresa_cod = item.find_all('p')[1]
-    empresa = list(empresa_cod)[2].strip()
-    empresas.append(empresa)
-    cod = list(empresa_cod)[6].strip()
-    cods.append(cod)
-print(cods)
-print('Depois do I')
-
-for i, v in enumerate(descricao):
-    print(f'{i + 1}º vaga')
-    print(f'{v}', end='\n\n')
+def filtrarPelaDescricao():
+    descricao = []
+    for item in soup.select('.texto'):
+        texto = item.find_all('p')[0]
+        descricao.append(texto)
+    return print(descricao)
