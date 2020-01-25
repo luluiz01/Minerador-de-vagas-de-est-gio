b11ce6ac894e1453dfcf13bd46c339bdb43f6a95
Luiz Henrique <42013630+luluiz01@users.noreply.github.com>
2020-01-25 15:09:04 -0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import funcoes
-funcoes.filtrarPeloTitulo()
-funcoes.filtrarPelaData()
-funcoes.filtrarPelaCidade()
-funcoes.filtrarPeloCodigo()
-funcoes.filtrarPeloLink()
-funcoes.filtrarPelaDescricao()
